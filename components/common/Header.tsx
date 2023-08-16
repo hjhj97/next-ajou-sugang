ec1dda489f753704ac21343e4e4485c0031db4f5
JuHeonHa <hjhjaueon97@naver.com>
2023-08-16 15:46:15 +0900

    feat : 헤더 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "@/styles/header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Link href="/">
+              <Image
+                src="http://sugang.ajou.ac.kr/images/white-logo.png"
+                layout="fill"
+                objectFit="contain"
+                alt="로고"
+              />
+            </Link>
+          </div>
+          <Link href="/notice">공지사항</Link>
+          <Link href="/apply">수강신청</Link>
+          <Link href="/my">개인시간표</Link>
+          <Link href="/changed">조정강의정보</Link>
+        </div>
+
+        <div className={styles.right}>
+          <p>김아주 / 201234567</p>
+          <button>로그아웃</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["sugang.ajou.ac.kr"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
